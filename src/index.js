009c2cb9332faa8be3d98a9f20c08604552e96ce
Oleksandr2891 <allavrenko@gmail.com>
2021-07-27 19:23:50 +0300

    Обновил package.json добавил при достижении 0 изменение цвета БГ

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './sass/main.scss';
 class CountdownTimer {
 
     constructor({ selector, targetDate }) {
+        this.intervalId = null;
         this.selector = selector;
         this.value = targetDate;
     }
@@ -10,13 +11,22 @@ class CountdownTimer {
     start(startTime) {
         startTime = this.value.getTime();
 
-        setInterval(() => {
+        this.intervalId = setInterval(() => {
+
             const currentTime = Date.now();
             const timeDelta = startTime - currentTime;
+            if (timeDelta <= 1) return this.stop();
             const time = this.getTimeComponents(timeDelta);
-            udateClockface(time)
+            this.udateClockface(time)
+
         }, 1000);
 
+    }
+
+    stop() {
+        clearInterval(this.intervalId);
+        const timerBg = document.querySelector(`body`);
+        timerBg.style.backgroundColor = 'red'
     }
 
     getTimeComponents(time) {
@@ -27,52 +37,24 @@ class CountdownTimer {
 
         return { days, hours, mins, secs };
     };
+    udateClockface({ days, hours, mins, secs }) {
+        const dayface = document.querySelector(`${this.selector} .date`);
+        const timeface = document.querySelector(`${this.selector} .time`);
+        dayface.textContent = `Days ${days}`;
+        timeface.textContent = `Time ${hours}:${mins}:${secs}`;
+    }
+
+
 };
 
 
 
 const counter = new CountdownTimer({
     selector: '#timer-1',
-    targetDate: new Date('Jul 17, 2022'),
+    targetDate: new Date('Jul 27, 2021 19:18'),
 });
 
 counter.start();
 
-// const countdownTimer = {
-
-//     start() {
-//         const startTime = new Date('Jul 17, 2022');
-
-//         setInterval(() => {
-//             const currentTime = Date.now();
-//             const timeDelta = startTime - currentTime;
-//             const time = getTimeComponents(timeDelta);
-//             udateClockface(time)
-//             console.log(timeComponents);
-
-//         }, 1000);
-
-//     }
-
-// };
-
-// countdownTimer.start();
-
-
-// function getTimeComponents(time) {
-//     const days = Math.floor(time / (1000 * 60 * 60 * 24)).toString().padStart(3, '0');
-//     const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)).toString().padStart(2, '0');
-//     const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)).toString().padStart(2, '0');
-//     const secs = Math.floor((time % (1000 * 60)) / 1000).toString().padStart(2, '0');
-
-//     return { days, hours, mins, secs };
-// };
-
-function udateClockface({ days, hours, mins, secs }) {
-    const dayface = document.querySelector('#timer-1 .date');
-    const timeface = document.querySelector('#timer-1 .time');
-    dayface.textContent = `Days ${days}`;
-    timeface.textContent = `Time ${hours}:${mins}:${secs}`;
-}
 
 
